e62aeadffb8c9e686c26b6ec74ab5949e74d56ae
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2025-01-11 16:17:19 +0200

    20250111

--- src/components/Buildings/Buildings.jsx
+++ src/components/Buildings/Buildings.jsx
@@ -1,37 +1,119 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel, Button } from '@chakra-ui/react';
-
+import { useState, useEffect } from 'react';
+import {
+  MapContainer,
+  // Marker,
+  // Popup,
+  TileLayer,
+  // GeoJSON,
+  LayersControl,
+  // Circle,
+  // useMap,
+  // CircleMarker,
+} from 'react-leaflet';
+import {
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from '@chakra-ui/react';
+import { GiNuclearWaste } from 'react-icons/gi';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { BuildingSpace, SelectedTab } from './Buildings.styled';
 export const Buildings = () => {
-  return (
-      <Tabs>          
-        <TabList>
-        <Button>Button</Button>      
-        <Tab>Building Schema</Tab>
-        <Tab>Measurment data</Tab>
-        <Tab>Photos</Tab>
-      </TabList>
+  const [pagePath, setPagePath] = useState();
+  console.log(pagePath);
+  
+  useEffect(() => {
+    setPagePath(window.location.search);
+  }, [setPagePath],)
+  
 
-      <TabPanels>
-        <TabPanel>
-          <Tabs>
-            <TabList>
-              <Tab>H = 0m</Tab>
-              <Tab>H = 1m</Tab>
-              <Tab>H = 2m</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>1</TabPanel>
-              <TabPanel>2</TabPanel>
-              <TabPanel>3</TabPanel>
-            </TabPanels>
-          </Tabs>
-        </TabPanel>
-        <TabPanel>
-          <p>two!</p>
-        </TabPanel>
-        <TabPanel>
-          <p>three!</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+  return (
+    <BuildingSpace>
+      <Tabs>
+        <TabList
+          display="flex"
+          alignItems="center"
+          justifyContent="space-around"
+        >
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<GiHamburgerMenu />}
+              variant="outline"
+            />
+            <MenuList fontSize="14px">
+              <MenuItem icon={<GiNuclearWaste />}>Building 1: </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 1"А": </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 1"Б"</MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 1"M"</MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 2: </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 2"Б":</MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 2"В": </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 2"Е"/1: </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 2"Е"/2: </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 3: </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 4-5: </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
+
+            </MenuList>
+          </Menu>
+          <SelectedTab>
+            <Tab>Scheme of the building</Tab>
+            <Tab>Measurment data</Tab>
+            <Tab>Photos</Tab>
+          </SelectedTab>
+          <div></div>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel height='100%' padding='0' margin='0'>
+            <MapContainer
+              center={[48.495, 34.68]}
+              zoom={17}
+              style={{
+                height: '80vh',
+                width: '100%',
+              }}
+              minZoom={14}
+              maxZoom={22}
+            >
+              <LayersControl position="topright">
+                <LayersControl.BaseLayer
+                  checked
+                  name="Mapbox Satellite"
+                  children=""
+                >
+                  <TileLayer
+                    attribution='&copy; <a href="https://www.mapbox.com">Mapbox</a> '
+                    url="https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}"
+                    accessToken={
+                      'pk.eyJ1IjoiMDAwMC0wMDAxLTgwMjUtODg4NSIsImEiOiJjbHFhdjNqY2ExZHZyMnJueHJmeXc1ZHduIn0.WsmLYujm4HrDa-K-VjJ2xA'
+                    }
+                  />
+                </LayersControl.BaseLayer>
+              </LayersControl>
+            </MapContainer>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </BuildingSpace>
   );
 };
---
+++ src/components/Buildings/Buildings.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+
+export const BuildingSpace = styled.div`
+width: 100%;
+height: 100%;
+`;
+
+export const SelectedTab = styled.div`
+display: flex;
+flex-direction: row;
+gap: 2px;    
+
+`;
