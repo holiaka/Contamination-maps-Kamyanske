af013d544f41cd838c192bc6cd153fac92405740
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2025-01-12 19:19:20 +0200

    20250112

--- src/components/Buildings/Buildings.jsx
+++ src/components/Buildings/Buildings.jsx
@@ -10,7 +10,7 @@ import {
   // useMap,
   // CircleMarker,
 } from 'react-leaflet';
-import {
+import {  
   IconButton,
   Menu,
   MenuButton,
@@ -22,9 +22,35 @@ import {
   Tab,
   TabPanel,
 } from '@chakra-ui/react';
-import { GiNuclearWaste } from 'react-icons/gi';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { BuildingSpace, SelectedTab } from './Buildings.styled';
+import { GiNuclearWaste,  GiHamburgerMenu, } from 'react-icons/gi';
+import { CiLineHeight } from "react-icons/ci";
+import { BuildingSpace, SelectedTab, MenuSpace, MapBox, MapInfo, MapTitle, MapLegend } from './Buildings.styled';
+
+let { origin, pathname } = window.location;
+let address = `${origin}${pathname}`;
+
+// const buildingData = 
+//   [
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1A", h: [-5.6, 0.0], address: ["b__1a_-5-6", "b__1a__0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1Б", h: [0.0, 9.0], address: ["b__1b__0-0", "b__1b__9-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1М", h: [-3.7, 0.0, 2,5], address: ["b__1m_-3-7", "b__1m__0-0", "b__1m__2-5"], center: [[48.495, 34.68], [48.495, 34.68], [48.495, 34.68]], zoom: [18, 18, 18] },
+//     { no: "2", h: [0.0, 2.2, 4.4, 6.8], address: ["b__2___0-0", "b__2___2-2", "b__2___4-4", "b__2___6-8"], center: [[48.495, 34.68], [48.495, 34.68], [48.495, 34.68], [48.495, 34.68]], zoom: [18, 18, 18, 18] },
+//     { no: "2Б", h: [-8.0, -4.0, 0.0, 5.0, 10.0, 15.0, 20.0, 24.0], address: ["b__2b_-8-0", "b__2b_-4-0", "b__2b__0-0", "b__2b__5-0", "b__2b_10-0", "b__2b_15-0", "b__2b_20-0", "b__2b_24-0"], center: [[48.495, 34.68], [48.495, 34.68], [48.495, 34.68], [48.495, 34.68], [48.495, 34.68], [48.495, 34.68],[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18, 18, 18, 18, 18, 18, 18] },
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//     { no: "1", h: [-4.0, 0.0], address: ["b__1__-4-0", "b__1___0-0"], center: [[48.495, 34.68], [48.495, 34.68]], zoom: [18, 18] },
+//   ];
+
+
+
+
 export const Buildings = () => {
   const [pagePath, setPagePath] = useState();
   console.log(pagePath);
@@ -42,6 +68,7 @@ export const Buildings = () => {
           alignItems="center"
           justifyContent="space-around"
         >
+          <MenuSpace>
           <Menu>
             <MenuButton
               as={IconButton}
@@ -49,8 +76,8 @@ export const Buildings = () => {
               icon={<GiHamburgerMenu />}
               variant="outline"
             />
-            <MenuList fontSize="14px">
-              <MenuItem icon={<GiNuclearWaste />}>Building 1: </MenuItem>
+            <MenuList fontSize="12px" padding='0'>
+              <MenuItem icon={<GiNuclearWaste />} as='a' href={`${address}?jgopfdjh`} >Building 1</MenuItem>
               <MenuItem icon={<GiNuclearWaste />}>Building 1"А": </MenuItem>
               <MenuItem icon={<GiNuclearWaste />}>Building 1"Б"</MenuItem>
               <MenuItem icon={<GiNuclearWaste />}>Building 1"M"</MenuItem>
@@ -61,14 +88,31 @@ export const Buildings = () => {
               <MenuItem icon={<GiNuclearWaste />}>Building 2"Е"/2: </MenuItem>
               <MenuItem icon={<GiNuclearWaste />}>Building 3: </MenuItem>
               <MenuItem icon={<GiNuclearWaste />}>Building 4-5: </MenuItem>
-              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
-              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
-              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
-              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
-              <MenuItem icon={<GiNuclearWaste />}>Building </MenuItem>
-
+              <MenuItem icon={<GiNuclearWaste />}>Building 6</MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 27</MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 28</MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 46</MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 112</MenuItem>
+              <MenuItem icon={<GiNuclearWaste />}>Building 120</MenuItem>
+            </MenuList>
+            </Menu>
+            
+            <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<CiLineHeight />}
+              variant="outline"
+            />
+            <MenuList fontSize="12px">
+              <MenuItem icon={<CiLineHeight />} as='a' href={`${address}?jgopfdjh`} >Building 1</MenuItem>
+              <MenuItem icon={<CiLineHeight />}>Building 1"А": </MenuItem>
+              <MenuItem icon={<CiLineHeight />}>Building 1"Б"</MenuItem>
+              <MenuItem icon={<CiLineHeight />}>Building 1"M"</MenuItem>
             </MenuList>
           </Menu>
+          </MenuSpace>
+          
           <SelectedTab>
             <Tab>Scheme of the building</Tab>
             <Tab>Measurment data</Tab>
@@ -79,14 +123,15 @@ export const Buildings = () => {
 
         <TabPanels>
           <TabPanel height='100%' padding='0' margin='0'>
-            <MapContainer
+            <MapBox>
+              <MapContainer
               center={[48.495, 34.68]}
-              zoom={17}
+              zoom={18}
               style={{
-                height: '80vh',
-                width: '100%',
+                height: 'auto',
+                width: '70%',
               }}
-              minZoom={14}
+              minZoom={17}
               maxZoom={22}
             >
               <LayersControl position="topright">
@@ -104,7 +149,19 @@ export const Buildings = () => {
                   />
                 </LayersControl.BaseLayer>
               </LayersControl>
-            </MapContainer>
+              </MapContainer>
+              <MapInfo>
+                <MapTitle>Building No  ${ }</MapTitle>
+                <MapLegend> LEGEND: </MapLegend>
+                
+              
+
+                
+              </MapInfo>
+
+
+            </MapBox>
+            
           </TabPanel>
           <TabPanel>
             <p>two!</p>
--- src/components/Buildings/Buildings.styled.jsx
+++ src/components/Buildings/Buildings.styled.jsx
@@ -6,9 +6,45 @@ width: 100%;
 height: 100%;
 `;
 
+export const MenuSpace = styled.div`
+display: flex;
+flex-direction: row;
+gap: 10px;    
+
+`;
+
 export const SelectedTab = styled.div`
 display: flex;
 flex-direction: row;
 gap: 2px;    
+
+`;
+
+export const MapBox = styled.div`
+display: flex;
+width: 100%;
+height: 78vh;
+
+`;
+
+export const MapInfo = styled.div`
+width: 30%;
+height: auto;
+padding: 10px;
+
+`;
+
+export const MapTitle = styled.h1`
+width: auto;
+font-size: 20px;
+text-align: center;
+font-weight: 700;
+`;
+
+export const MapLegend = styled.h2`
+width: auto;
+font-size: 16px;
+text-align: center;
+font-weight: 500;
 
 `;
